1b375d94431f1371ebe343ab3bec63a308373b80
Alexandre Cirilo <38657258+alxdrcirilo@users.noreply.github.com>
2023-08-06 19:14:14 +0200

    feat: set logging level to INFO

--- checkers/utils/logging.py
+++ checkers/utils/logging.py
@@ -1,7 +1,7 @@
 import logging
 
 logging.basicConfig(
-    level=logging.CRITICAL,
+    level=logging.INFO,
     format="%(asctime)s - %(levelname)s: %(message)s",
     handlers=[logging.StreamHandler(), logging.FileHandler("checkers.log", mode="w")],
 )
